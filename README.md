0efef8a8f9815ff8d4f2cd8fed140cb79a42f3c3
GhobiPC <ghobikan15@hotmail.co.uk>
2024-01-10 21:06:41 +0000

    hangman

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
+# Prerequsiites
+
+Before doing the hangman task, there was some milestones and subtasks that had to be done. This included the following:
+ 
+Creating a word bank with fruits and making the user input a letter of the randomly selected fruit. If the letter corresponds with any of the letters in the word, the user is given a print statement saying that the letter is correct. 
+
+
 # Hangman
 Hangman is a classic game in which a player thinks of a word and the other player tries to guess that word within a certain amount of attempts.
 
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
 
-ih
--- milestone_2.py
+++ milestone_2.py
@@ -1,1 +1,25 @@
+# Imports
+import random
 
+# Creating a List of Fruits
+word_list = ['apple', 'banana', 'orange', 'grape']
+
+print("Word List:", word_list)
+
+# Assigning a randomly selected word from the list to a variable called "word"
+word = random.choice(word_list)
+
+# Print the randomly selected word
+print("Randomly selected word:", word)
+
+# Taking user input
+guess = input("Enter a single letter: ")
+
+# Validating user input
+if len(guess) == 1 and guess.isalpha():
+    if guess in word:
+        print("The guess is correct! The letter '{}' is in the word.".format(guess))
+    else:
+        print("The guess is incorrect. The letter '{}' is not in the word.".format(guess))
+else:
+    print("Invalid input. Please enter a single letter.")
